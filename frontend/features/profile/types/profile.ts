445d8934c173d06c2279b8edaf194e50bb11bb6f
RobLaran <160157653+RobLaran@users.noreply.github.com>
2026-07-15 23:13:55 +0800

    refactor: replace to first name and last name instead of full name

--- frontend/features/profile/types/profile.ts
+++ frontend/features/profile/types/profile.ts
@@ -1,6 +1,7 @@
-export type Profile = {
+export interface Profile {
     id: string;
-    name: string;
+    firstName: string;
+    lastName: string;
     email: string;
     phone: string | null;
     location: string | null;
@@ -9,18 +10,7 @@ export type Profile = {
     avatar: string | null;
     ipAddress: string;
     userAgent: string;
-};
-
-export type EditedProfile = Profile;
-
-export type LoginLog = {
-    login_log_id: string;
-    created_at: string;
-    ip_address: string;
-    user_agent: string;
-    status: string;
-};
-
+}
 export interface UpdateCurrentUserValues {
     firstName: string;
     lastName: string;
