4b4449b5fd30c7c52015b6dbd0cd89a9ef059f08
RobLaran <160157653+RobLaran@users.noreply.github.com>
2026-07-15 20:38:02 +0800

    feat(login log service): serialize login log before returning

--- backend/app/services/login_log_service.py
+++ backend/app/services/login_log_service.py
@@ -1,5 +1,6 @@
 from app.repositories.login_log_repository import get_all_login_logs, get_user_last_login, get_user_login_logs
-from app.types.login_log_types import LoginLog
+from app.types.login_log_types import LoginLog, SerializedLoginLog
+from app.serializers.login_log_serializer import serialize_login_log
 
 
 # get all login logs
@@ -11,11 +12,11 @@ def get_all_login_history() -> list[LoginLog]:
 # Log login failed
 
 # get login history
-def get_login_history(user_id: str) -> list[LoginLog]:
+def get_login_history(user_id: str) -> list[SerializedLoginLog]:
     login_history = get_user_login_logs(user_id)
-    return login_history
+    return [serialize_login_log(log) for log in login_history]
 
 # get last login
-def get_last_login(user_id: str) -> LoginLog:
+def get_last_login(user_id: str) -> SerializedLoginLog:
     last_login = get_user_last_login(user_id)
-    return last_login
+    return serialize_login_log(last_login)
